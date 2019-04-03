5bc895623e1bd2eb780f07b55046d18755883398
Travis Rollins <troll59910@aol.com>
2019-04-03 13:55:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Train
+
+## Installation Steps
+
+1. Clone the repo
+2. From the cloned directory, run `npm install`
+3. Run your first test suite `npm test airport-test.js` 
+4. Make the tests pass
+
---
+++ train-test.js
@@ -0,0 +1,75 @@
+var assert = require('chai').assert;
+var Train = require('./train')
+
+describe('Train', function() {
+  it.skip('should be a function and create a train', function(){
+    assert.isFunction(Train);
+  });
+
+  it.skip('should create a train', function() {
+    var train = new Train();
+        
+    assert.isObject(train);
+  });
+
+  it.skip('should not have passengers by default', function() {
+    var train = new Train('Orient Express');
+
+    assert.deepEqual(train.passengers, []);
+  });
+
+  it.skip('should be able to have passengers at the beginning of the ride', function() {
+    var train1 = new Train('Orient Express', ['Louisa', 'Travis', 'Khalid']);
+    var train2 = new Train('Trans-Siberian Express', ['Pam', 'Robbie']);
+
+    assert.deepEqual(train1.passengers, ['Louisa', 'Travis', 'Khalid']);
+
+    assert.deepEqual(train2.passengers, ['Pam', 'Robbie']);
+  });
+
+  it.skip('should have a number of seats available', function(){
+    var train1 = new Train('Denver Light Rail', [], 100);
+    var train2 = new Train('Orient Express', ['Louisa', 'Travis', 'Khalid'], 60);
+    var train3 = new Train('Trans-Siberian Express', ['Pam', 'Robbie'], 120);
+
+   assert.equal(train1.seatsAvailable, 100);
+   assert.equal(train2.seatsAvailable, 57);
+   assert.equal(train3.seatsAvailable, 118);
+  });
+
+  it.skip('should be able to pick up passengers', function() {
+    var train = new Train('Denver Light Rail', ['Louisa', 'Travis', 'Khalid']);
+
+    var pepsiCenter = { boarding: ['Robbie', 'David', 'Will'] };
+
+    train.stopAtStation(pepsiCenter);
+
+    assert.equal(train.passengers.length, 6);
+    assert.deepEqual(train.passengers, [
+      'Louisa',
+      'Travis',
+      'Khalid',
+      'Robbie',
+      'David',
+      'Will'
+    ]);
+  });
+
+  it.skip('should be able to drop off passengers', function() {
+    var train = new Train('Denver Light Rail', ['Louisa', 'Travis', 'Khalid']);
+
+    var pepsiCenter = { boarding: ['Christie', 'Brittany', 'Leta'] };
+
+    train.stopAtStation(pepsiCenter, true);
+
+    assert.deepEqual(train.passengers, []);
+    assert.deepEqual(pepsiCenter.boarding, [
+      'Christie',
+      'Brittany',
+      'Leta',
+      'Louisa',
+      'Travis',
+      'Khalid'
+    ]);
+  });
+})
